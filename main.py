ee72fb51d1778a77b5fffe87182878cd077f483f
FredrikBakken <fredda10x@gmail.com>
2017-09-09 22:46:04 +0200

    Known issues.
    Issues related to encoding/decoding data from PDF to csv.

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from db import db_delete, db_insert, db_search_ticker
-
 from reader import covert_pdf, store_stocks
-
 from scrape import download, scrape, status
-
 
 def init():
     # Scrape pdf and check age
